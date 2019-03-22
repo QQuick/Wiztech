b150ff67455d8ed175875fda256c254ff7c48809
Jacques de Hooge <info@geatec.com>
2019-03-22 11:11:22 +0100

    Les Python in command prompt klaar

---
+++ inleiding_programmeren_in_python/les_2_if_else/progs/prog_02_name.py
@@ -0,0 +1,3 @@
+print ('Hallo, ik ben je computer!')
+name = input ('Hoe heet je? ')
+print (f'Dag {name}, welkom bij deze Python les!')
---
+++ inleiding_programmeren_in_python/les_2_if_else/progs/prog_04_age.py
@@ -0,0 +1,7 @@
+age = int (input ('Hoe oud ben je? '))
+
+if age < 18:
+    print (f'Jammer, {age} is te jong voor de sigarenrokers-vereniging.')
+else:
+    print (f'Jammer, {age} is te oud voor een fopspeen.')
+    
